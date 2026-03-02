cba0f69a9ad1e957a3626e97a638a80d86175fc2
Domanico2405 <edrianepauldomanico@gmail.com>
2026-03-02 13:46:33 +0800

    'jessie'

--- week5-6/accounts/urls.py
+++ week5-6/accounts/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(tempalte_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LoginView.as_view(), name='logut'),
     path('register/', views.register, name='register'),
     path('dashboard/', views.dashboard, name='dashboard'),
