e1f7b6499a83f517f930eed1bd14c53ceee4f3d3
Domanico2405 <edrianepauldomanico@gmail.com>
2026-03-02 13:27:56 +0800

    'march2'

--- week-3-4/apps/projects/models.py
+++ week-3-4/apps/projects/models.py
@@ -37,4 +37,4 @@ class Project(models.Model):
         self.save()
     
     def __str__(self):
-        return self.title
+        return self                                                                                                                            .title
